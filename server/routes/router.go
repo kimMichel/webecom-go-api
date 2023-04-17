1d3c651d3cf066572540f74e596832d7c6f783f6
Michel Kim <kim98.michel@gmail.com>
2023-04-17 20:25:12 -0300

    configure server with gin

---
+++ server/routes/router.go
@@ -0,0 +1,13 @@
+package routes
+
+import "github.com/gin-gonic/gin"
+
+func ConfigRoutes(router *gin.Engine) *gin.Engine {
+	main := router.Group("api/v1")
+	{
+		products := main.Group("products")
+		{
+			products.GET("/")
+		}
+	}
+}
