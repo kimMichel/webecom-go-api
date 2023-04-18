e11789274de654d0941f911626c25a7a5c45baa0
Michel Kim <kim98.michel@gmail.com>
2023-04-17 21:00:39 -0300

    configure database

---
+++ database/database.go
@@ -0,0 +1,43 @@
+package database
+
+import (
+	"fmt"
+	"log"
+	"os"
+	"time"
+
+	"gorm.io/driver/postgres"
+	"gorm.io/gorm"
+)
+
+var db *gorm.DB
+
+func ConnectDb() {
+	dsn := fmt.Sprintf(
+		"host=%s user=%s password=%s dbname=%s port=5432 sslmode=disable",
+		os.Getenv("DB_HOST"),
+		os.Getenv("DB_USER"),
+		os.Getenv("DB_PASSWORD"),
+		os.Getenv("DB_NAME"),
+	)
+
+	database, err := gorm.Open(postgres.Open(dsn), &gorm.Config{})
+
+	if err != nil {
+		log.Fatal("error: ", err)
+	}
+
+	db = database
+
+	config, _ := db.DB()
+
+	config.SetMaxIdleConns(10)
+	config.SetMaxOpenConns(100)
+	config.SetConnMaxLifetime(time.Hour)
+
+	log.Println("Connected to database...")
+}
+
+func GetDatabase() *gorm.DB {
+	return db
+}
--- main.go
+++ main.go
@@ -1,5 +1,13 @@
 package main
 
-func main() {
+import (
+	"github.com/kimMichel/webecom-go-api/database"
+	"github.com/kimMichel/webecom-go-api/server"
+)
 
+func main() {
+	database.ConnectDb()
+
+	server := server.NewServer()
+	server.Run()
 }
